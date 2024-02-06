040ab68f9c622e406a76eb7bcf85a4d6894cb35b
Eereti Siva Nagaraju <nagarajeereti@edvenswatech.com>
2024-02-06 15:51:24 +0530

    delete the row from data table

--- force-app/main/default/lwc/MyComponents/lwc/statusChanger/statusChanger.js
+++ force-app/main/default/lwc/MyComponents/lwc/statusChanger/statusChanger.js
@@ -1,106 +1,128 @@
-import { LightningElement, wire } from 'lwc';
+import { LightningElement, track, wire } from 'lwc';
 import BOOK_OBJECT from '@salesforce/schema/Book__c'
 import { getListUi } from 'lightning/uiListApi';
 import { refreshApex } from '@salesforce/apex';
-import { createRecord, updateRecord } from 'lightning/uiRecordApi';
-const COLS =[
+import { createRecord, deleteRecord, updateRecord } from 'lightning/uiRecordApi';
+const COLS = [
     { label: "Id", fieldName: "Id" },
-    { label: "Name", fieldName: "Name",editable: true },
+    { label: "Name", fieldName: "Name", editable: true },
     // { label: "Title", fieldName: "Title" },
     // { label: "Phone", fieldName: "Phone", editable: true },
-    { label: "isReaded", fieldName: "isReaded__c",type: 'boolean', editable: true },
+    { label: "isReaded", fieldName: "isReaded__c", type: 'boolean', editable: true },
     {
         label: 'Action',
         type: 'button',
         initialWidth: 135,
         typeAttributes: {
             label: { fieldName: 'ButtonLabel' },
-            title: 'Click to perform an action',
+            title: 'Create new Book',
             name: 'actionButton',
             value: 'clickAction',
             variant: { fieldName: 'ButtonColor' },
-            
+
+        }
+    },
+    {
+        label: 'Delete',
+        type: 'button',
+        initialWidth: 135,
+        typeAttributes: {
+            label: "Delete",
+            title: 'Click to delete row',
+            name: 'delete',
+            value: 'clickAction',
+            variant: "destructive",
+
         }
     }
 ]
 export default class StatusChanger extends LightningElement {
 
-    allEmployees = []
-    error=''
+    @track allEmployees = [];
+    error = ''
     columns = COLS;
     draftValues = [];
     newBook = false;
     formFields = {};
 
-    connectedCallback(){
-        this.loadData();
-    }
-    @wire(getListUi, { objectApiName: BOOK_OBJECT.objectApiName,listViewApiName:"All" })
+    // connectedCallback(){
+    //     this.loadData();
+    // }
+    @wire(getListUi, { objectApiName: BOOK_OBJECT.objectApiName, listViewApiName: "All" })
     getAllEmployees({ error, data }) {
-        if (error) { 
+        if (error) {
             console.log(error)
         }
-        if(data){
-            console.log(data,"new emps data")
-            this.allEmployees = data.records.records.map(emp=>{
+        if (data) {
+            console.log(data, "new emps data")
+            this.allEmployees = data.records.records.map(emp => {
                 return {
-                    "Name":this.getValue(emp,"Name"),
-                    "Id" :this.getValue(emp,"Id"),
-                    "isReaded__c" :this.getValue(emp,"isReaded__c"),
-                    "ButtonLabel" :this.getValue(emp,"isReaded__c")? "Unread" :"Readed",
-                    "ButtonColor" :this.getValue(emp,"isReaded__c")? "destructive":"brand",
+                    "Name": this.getValue(emp, "Name"),
+                    "Id": this.getValue(emp, "Id"),
+                    "isReaded__c": this.getValue(emp, "isReaded__c"),
+                    "ButtonLabel": this.getValue(emp, "isReaded__c") ? "Unread" : "Readed",
+                    "ButtonColor": this.getValue(emp, "isReaded__c") ? "destructive" : "brand",
                 }
-            })            
+            })
         }
     }
-    newBookhandler(){
+    newBookHandler() {
         this.newBook = !this.newBook
     }
-    bookInputHandler(event){
-        let {name,value,} = event.target;
+    bookInputHandler(event) {
+        let { name, value, } = event.target;
         this.formFields[name] = value;
-        name == 'isReaded__c' ? this.formFields[name] = event.target.checked:null;
+        name == 'isReaded__c' ? this.formFields[name] = event.target.checked : null;
     }
-    createNewBook(){
-        createRecord({apiName: BOOK_OBJECT.objectApiName,fields:this.formFields})
-        .then((result)=>{
-           this.loadData();
-            this.allEmployees.push(result.fields)
-            console.log("new Book created",result)
-            this.formFields = {};
-            this.template.querySelector('form.createBook').reset()
-        })
-        .catch((err)=>{
-            console.log("new Book err",err)
-        })
+    createNewBook() {
+        createRecord({ apiName: BOOK_OBJECT.objectApiName, fields: this.formFields })
+            .then((result) => {
+                this.formFields = {};
+                this.template.querySelector('form.createBook').reset();
+            })
+            .catch((err) => {
+                console.log("new Book err",)
+            })
     }
     getValue(data, field) {
         return data.fields[field].value
     }
-    handleRowAction(event){
+    handleRowAction(event) {
         const action = event.detail.action;
         const row = event.detail.row
 
-        if(action.name == 'actionButton'){
+        if (action.name == 'actionButton') {
             this.handleButtonClick(row);
-            
+
         }
-       
+        if (action.name == 'delete') {
+            this.deleteHandle(row);
+            console.log("delete function")
+
+        }
     }
-    handleButtonClick(row){
-       const updatedRow = {"Id":row.Id,"isReaded__c":!row.isReaded__c}
-       updateRecord({fields:updatedRow})
-       .then(result=>{
-        console.log(result,"result")
-           this.allEmployees = this.allEmployees.map(item=>(item.Id == row.Id ?{...item,...updatedRow}:item ))
-       })
-       .catch((err)=>{
-        console.log(err,"err",70)
-       })
+    async deleteHandle(row) {
+        try {
+            let result = deleteRecord(row.Id)
+            console.log(result, "delete row try")
+        }
+        catch (err) {
+            console.log(err, "error in delete")
+        }
+    }
+    handleButtonClick(row) {
+        const updatedRow = { "Id": row.Id, "isReaded__c": !row.isReaded__c }
+        updateRecord({ fields: updatedRow })
+            .then(result => {
+                console.log(result, "result")
+                this.allEmployees = this.allEmployees.map(item => (item.Id == row.Id ? { ...item, ...updatedRow } : item))
+            })
+            .catch((err) => {
+                console.log(err, "err", 70)
+            })
     }
     handleSave(event) {
-    
-        console.log(event, "handle save event",event.detail.draftValues)
+        console.log(event, "handle save event", event.detail.draftValues)
         const recordInputs = event.detail.draftValues.map(draft => {
             const fields = { ...draft }
             return { fields: fields }
@@ -109,17 +131,17 @@ export default class StatusChanger extends LightningElement {
 
         Promise.all(promises)
             .then((result) => {
-                console.log("book updated ",result)
+                console.log("book updated ", result)
                 this.draftValues = [];
             })
             .catch((err) => {
                 console.error(err, "data table error")
             })
-    
+
     }
     loadData() {
-        // Refresh the wired data
-        return refreshApex(this.getAllEmployees);
+        console.log("Refresh the wired data");
+        return refreshApex(this.allEmployees);
     }
 
 }
